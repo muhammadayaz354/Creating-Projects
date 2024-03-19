895f898f092167d27d5edfe048813ca1a6e98eb7
muhammadayaz354 <160526605+muhammadayaz354@users.noreply.github.com>
2024-03-19 21:47:45 +0500

    A Simple Calculator

---
+++ app.ts
@@ -0,0 +1,32 @@
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt([
+  {
+    message: "please select your first number",
+    type: "number",
+    name: "firstNumber",
+  },
+  {
+    message: "please select your Second number",
+    type: "number",
+    name: "SecondNumber",
+  },
+  {
+    message: "please select any one operator to perform Action",
+    type: "list",
+    name: "Operator",
+    choices: ["Addition", "Subtraction", "Multiplication", "Devision"],
+  },
+]);
+
+if (answer.Operator === "Addition") {
+  console.log(answer.firstNumber + answer.SecondNumber);
+} else if (answer.Operator === "Subtraction") {
+  console.log(answer.firstNumber - answer.SecondNumber);
+} else if (answer.Operator === "Multiplication") {
+  console.log(answer.firstNumber * answer.SecondNumber);
+} else if (answer.Operator === "Devision") {
+  console.log(answer.firstNumber / answer.SecondNumber);
+} else {
+  ("Please Select any valid number");
+}
